b3b653b7105ef18975a0cbc56ec73cc1bac90017
Bot_Ninja <85632248+Hydra-Fire@users.noreply.github.com>
2022-08-14 01:40:40 +0900

    Update ninja_gram.sh

--- ninja_gram.sh
+++ ninja_gram.sh
@@ -2,7 +2,7 @@ echo ""
 echo ""
 echo ""
 echo "ARE YOU READY FOR START NOW"
-echo "INSTALATION NINJAGRAM"
+echo "INSTALATION NINJA_GRAM 2022"
 echo "GET UP FOLLOWERS"
 echo "JUST ONE CLICK"
 echo "LET'S SUBSCRIBE FOR SUPPORT"
@@ -24,62 +24,21 @@ apt-get install openssh -y
 apt-get install openssl -y
 apt-get install nodejs -y
 apt-get install npm -y
-pt-get install unzip -y
+apt-get install unzip -y
 apt-get install python2 -y
 apt-get install python3 -y
 unzip ninja_gram.zip
 python -m pip install --upgrade pip
-pip install python-facebook-api
-pip install bootstrap -v 5.0.0.alpha3
 pip install -U instabot
-pip install clang
 pip install selenium
 pip install colorama
 pip install twine
-pip install mechanize
-pip install pyramid
-pip install falcon
-pip install eve
-pip install CherryPy
-pip install tornado
-pip install tox-travis codecov
-pip install python-instagram
-pip install django-social-auth
-pip install py-oauth2
-pip install CProxy
-pip install signalfx-tracing
-pip install sqlite-utils
-pip install django_social_followers
-pip install prometheus-client
-pip install django-smart-proxy
-pip install InstagramGraphQL
-pip install instagram-api
-pip install instagram-oauth
-pip install instagram-sdk
 pip install instagram_private_api
-pip install configparser
-pip install Flask
-pip install Flask-RESTful
-pip install redis
-pip install rq
-pip install marshmallow
-pip install flake8
-pip install Flask-Sessionstore
-pip install Flask-SQLAlchemy
-pip install flask-talisman
-pip install pysaml2
-pip install flask-Login
-pip install Flask-Cors
-pip install Flask-SeaSurf
 pip install git+https://git@github.com/ping/instagram_private_api.git@1.6.0 --upgrade
 python3 -c "$(curl https://gist.github.com/vsajip/4673395/raw/3420d9150cele9797dc8522fce7386d8643b02al/pyenv.py)" env-dir
-curl -O https://nodejs.org/dist/v12.8.1-1/SHASUMS256.txt.sig
 npm install -g npm
-git clone https://github.com/pormes/npm-upgrade.git
-cd npm-upgrade
-npm install -g npm-upgrade
+npm install instagram-private-api
 npm install
-cd ../
 npm install npm@latest
 npm i instabot
 npm i http-proxy-agent
@@ -91,7 +50,6 @@ npm i instagram-private-api
 npm i bootstrap@5.0.0-alpha1
 npm install bootstrap@next
 npm install
-npm-upgrade
 echo ""
 echo "DON'T FORGET TO SUBSCRIBE"
 echo ""
